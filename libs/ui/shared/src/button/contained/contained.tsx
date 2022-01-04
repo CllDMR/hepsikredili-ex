1e618511cd8a6a693ba4dfeb2b954167b0223e6a
CllDMR <celal.demirx@gmail.com>
2022-01-04 22:53:12 +0300

    fixed button

--- libs/ui/shared/src/button/contained/contained.tsx
+++ libs/ui/shared/src/button/contained/contained.tsx
@@ -4,11 +4,23 @@ import './contained.module.css';
 
 export interface ButtonContainedProps extends ButtonBaseProps {
   variant: 'contained';
+  buttonRef?: React.ForwardedRef<HTMLButtonElement>;
 }
 
-export const ButtonContained: FC<ButtonContainedProps> = (props) => (
-  <button className="bg-red-900 border-8 border-green-800 ">
-    <span>{props.title}</span>
+export const ButtonContained: FC<ButtonContainedProps> = ({
+  title,
+  fullWidth = false,
+  buttonRef,
+  onClick,
+}) => (
+  <button
+    className={`disabled:bg-gray-500 text-white hover:bg-indigo-600 min-w-[128px] border transition duration-150 ease-in-out border-gray-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-opacity-75 px-4 py-2 bg-indigo-500 rounded ${
+      fullWidth && 'w-full'
+    }`}
+    ref={buttonRef}
+    onClick={onClick}
+  >
+    <span className="whitespace-nowrap">{title}</span>
   </button>
 );
 
