9ea3b0df05fa3486d23cdc007726db97bbb338fb
CllDMR <celal.demirx@gmail.com>
2022-01-05 20:54:24 +0300

    generated web-customer auth pages

---
+++ apps/web-customer/pages/auth/login/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+/* eslint-disable-next-line */
+export interface PageLoginProps {}
+
+export const Login: FC<PageLoginProps> = (props) => (
+  <div>
+    <h1>Welcome to Login!</h1>
+  </div>
+);
+
+export default Login;
---
+++ apps/web-customer/pages/auth/register/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+/* eslint-disable-next-line */
+export interface PageRegisterProps {}
+
+export const Register: FC<PageRegisterProps> = (props) => (
+  <div>
+    <h1>Welcome to Register!</h1>
+  </div>
+);
+
+export default Register;
